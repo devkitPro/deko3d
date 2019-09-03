777f43b0d235b38755e44d85c9d5deec71ad6834
fincs <fincs@devkitpro.org>
2019-09-03 17:24:48 +0200

    Fix bugs in dk::detail::BlitCopyEngine

--- source/maxwell/gpu_transfer.cpp
+++ source/maxwell/gpu_transfer.cpp
@@ -26,7 +26,7 @@ void tag_DkQueue::setupTransfer()
 void dk::detail::BlitCopyEngine(DkCmdBuf obj, ImageInfo const& src, ImageInfo const& dst, uint32_t srcX, uint32_t srcY, uint32_t dstX, uint32_t dstY, uint32_t width, uint32_t height)
 {
 	CmdBufWriter w{obj};
-	w.reserve((!src.m_isLinear+!dst.m_isLinear)*7 + 4 + 10);
+	w.reserve(2*7 + 4 + 10);
 
 	DkGpuAddr srcIova = src.m_iova;
 	DkGpuAddr dstIova = dst.m_iova;
@@ -43,35 +43,48 @@ void dk::detail::BlitCopyEngine(DkCmdBuf obj, ImageInfo const& src, ImageInfo co
 	else if (dst.m_widthMs * dst.m_bytesPerBlock > 0x10000)
 		useSwizzle = true;
 
+	uint32_t srcHorizFactor = 1;
+	uint32_t dstHorizFactor = 1;
+
 	if (!useSwizzle)
 	{
 		srcX *= src.m_bytesPerBlock;
 		dstX *= dst.m_bytesPerBlock;
 		width *= src.m_bytesPerBlock;
+		srcHorizFactor = src.m_bytesPerBlock;
+		dstHorizFactor = dst.m_bytesPerBlock;
 	}
 
 	if (!src.m_isLinear)
 	{
-		copyFlags |= Copy::Execute::SrcIsBlockLinear{};
 		w << Cmd(Copy, SrcTileMode{},
 			src.m_tileMode | 0x1000,
-			src.m_horizontal,
+			src.m_horizontal*srcHorizFactor,
 			src.m_vertical,
 			1U << ((src.m_tileMode >> 8) & 0xF),
 			0,
 			Copy::SrcPosXY::X{srcX} | Copy::SrcPosXY::Y{srcY});
 	}
+	else
+	{
+		copyFlags |= Copy::Execute::SrcIsPitchLinear{};
+		w << Cmd(Copy, SrcPitch{}, src.m_horizontal);
+	}
 
 	if (!dst.m_isLinear)
 	{
-		copyFlags |= Copy::Execute::DstIsBlockLinear{};
 		w << Cmd(Copy, DstTileMode{},
 			dst.m_tileMode | 0x1000,
-			dst.m_horizontal,
+			dst.m_horizontal*dstHorizFactor,
 			dst.m_vertical,
 			1U << ((dst.m_tileMode >> 8) & 0xF),
 			0,
 			Copy::DstPosXY::X{dstX} | Copy::DstPosXY::Y{dstY});
+	}
+	else
+	{
+		copyFlags |= Copy::Execute::DstIsPitchLinear{};
+		w << Cmd(Copy, DstPitch{}, dst.m_horizontal);
 	}
 
 	if (useSwizzle)
@@ -96,6 +109,7 @@ void dk::detail::BlitCopyEngine(DkCmdBuf obj, ImageInfo const& src, ImageInfo co
 
 void dk::detail::Blit2DEngine(DkCmdBuf obj, ImageInfo const& src, ImageInfo const& dst, uint32_t srcX, uint32_t srcY, uint32_t dstX, uint32_t dstY, uint32_t dstW, uint32_t dstH, uint32_t dudx, uint32_t dvdy, uint32_t fractBits, uint32_t flags)
 {
+	// TODO
 }
 
 void dkCmdBufPushData(DkCmdBuf obj, DkGpuAddr addr, const void* data, uint32_t size)
